e1ad16d42cda6975e3d8dc26b24397b675bef722
Alan Brazões <alanbrazoes@gmail.com>
2022-06-17 12:18:55 -0300

    fix: remove functions and types

--- src/services/dishesApi.ts
+++ src/services/dishesApi.ts
@@ -1,31 +1,9 @@
 import api from '@services/index';
-import { ICreateDishes } from '@types';
 
 export const getAllDishes = async () => {
   try {
     const { data, status } = await api.get('/dishes');
     return { data, status };
-  } catch (error) {
-    return false;
-  }
-};
-
-export const createDishe = async ({
-  name,
-  price,
-  preparationTime,
-  ingredientList,
-  type,
-}: ICreateDishes) => {
-  try {
-    const response = await api.post('/create/dishes', {
-      name,
-      price,
-      preparationTime,
-      ingredientList,
-      type,
-    });
-    return response.status;
   } catch (error) {
     return false;
   }
--- src/services/drinksApi.ts
+++ src/services/drinksApi.ts
@@ -1,5 +1,4 @@
 import api from '@services/index';
-import { IDrink } from '@types';
 
 export default {
   async getAllDrinks() {
@@ -15,38 +14,6 @@ export default {
     try {
       const response = await api.get(`/drink/${id}`);
       return response.data;
-    } catch (error) {
-      return false;
-    }
-  },
-
-  async createDrink({ name, price }: IDrink) {
-    const body = {
-      name,
-      price,
-    };
-
-    try {
-      await api.post('/create/drink', body);
-      return true;
-    } catch (error) {
-      return false;
-    }
-  },
-
-  async updateDrink({ name, price, _id }: IDrink) {
-    try {
-      await api.put(`/drink/update/${_id}`, { name, price });
-      return true;
-    } catch (error) {
-      return false;
-    }
-  },
-
-  async deleteDrink(id: string) {
-    try {
-      await api.delete(`/drink/delete/${id}`);
-      return true;
     } catch (error) {
       return false;
     }
